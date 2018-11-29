9716e8faebef01e48c144df028fdfb3aabbe530a
Emily Asch <e.asch@me.com>
2018-11-29 16:19:29 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears VALUES(1,'Mr. Chocolate',2,'Male','brown','fine',1);
+
+INSERT INTO bears VALUES(2,'Rowdy',4,'Male','black','senstive',0);
+
+INSERT INTO bears VALUES(3,'Tabitha',3,'F','brown','loving',1);
+
+INSERT INTO bears VALUES(4,'Sergeant Brown',7,'Male','black','patient',1);
+
+INSERT INTO bears VALUES(5,'Melissa',1,'F','white','fiesty',1);
+
+INSERT INTO bears VALUES(6,'Grinch',2,'male','black','calm',0);
+
+INSERT INTO bears VALUES(7,'Wendy',3,'F','brown','goofy',1);
+
+INSERT INTO bears VALUES(8,NULL,4,'Male','brown','aggressive',1);
+
+
